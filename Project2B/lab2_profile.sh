75f1c774f0c737de99736a223049fb1ff7c8d2e6
Prithvi Kannan <prithvi.kannan@gmail.com>
2020-02-19 14:21:54 -0800

    Updated profile script

--- Project2B/lab2_profile.sh
+++ Project2B/lab2_profile.sh
@@ -4,10 +4,10 @@
 #EMAIL: prithvi.kannan@gmail.com
 #ID: 405110096
 
-rm -f profile.gperf profile.out
+rm -f profile.out
 LD_PRELOAD=/usr/lib64/libprofiler.so \
-CPUPROFILE=./raw.gperf \
+CPUPROFILE=./temp.gperf \
 ./lab2_list --iterations=1000 --threads=12 --sync=s
-pprof --text ./lab2_list ./raw.gperf > profile.out
-pprof --list=newThreadFunction ./lab2_list ./raw.gperf >> profile.out
-rm -f ./raw.gperf
+pprof --text ./lab2_list ./temp.gperf > profile.out
+pprof --list=newThreadFunction ./lab2_list ./temp.gperf >> profile.out
+rm -f ./temp.gperf
